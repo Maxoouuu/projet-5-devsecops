4bc0097a99b1d6c083f9fde3100a32ed6846b591
Maxime crt <maxime.coriton@gmail.com>
2023-12-22 14:36:55 +0100

    doc

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-//App.tsx
+// Path : src/App.tsx
 import React from "react";
 import Dashboard from "./Dashboard";
 import "./App.css"; // Assurez-vous d'avoir un fichier CSS pour les styles généraux si nécessaire.
